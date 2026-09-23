2f83ae5396628dce4a413a9e01cbcdd51adb9192
Franck Porteous <franck.porteous@protonmail.com>
2026-09-23 13:39:57 -0400

    (cleanup) removing the proxy middleware causing Supabase round trip on any request

--- src/lib/supabase/server.ts
+++
@@ -1,7 +0,0 @@
-import { cookies } from "next/headers";
-import { createClient as createServerSupabase } from "@/utils/supabase/server";
-
-export async function createClient() {
-  const cookieStore = await cookies();
-  return createServerSupabase(cookieStore);
-}
